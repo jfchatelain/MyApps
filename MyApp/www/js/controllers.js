081db183b905b779ee50470116b1bdbfb9a169a1
Jean-Francois Chatelain <jfchatelain@hotmail.com>
2016-02-10 18:07:28 -0500

    Introduces actions

--- MyApp/www/js/controllers.js
+++ MyApp/www/js/controllers.js
@@ -1,11 +1,51 @@
+ 
+
+var myWeather = function (myScope, getCurrentPosition,getWeather) { 
+      getCurrentPosition(function (position) {
+            getWeather(
+               position
+               , function (data, position) {
+                   console.log(data);
+                   myScope.City = data.name;
+                   myScope.Country = data.sys.country;
+                   myScope.Description = data.weather[0].description;
+                   myScope.IconId = data.weather[0].icon;
+                   myScope.Temperature = data.main.temp;
+                   myScope.Time = Date.now();
+                   myScope.WindSpeed = data.wind.speed;         
+                  
+                   console.log('Weather retreived')
+
+               });
+
+        });
+}
+
 angular.module('app.controllers', [])
-     
-.controller('weatherCtrl', function($scope) {
-
-})
+.controller('weatherCtrl', [ '$scope', 'getCurrentPosition', 'getWeather', function($scope, getCurrentPosition, getWeather) {
+    myWeather($scope, getCurrentPosition, getWeather);
+     $scope.refresh =   function() { myWeather($scope, getCurrentPosition,getWeather);} ;
+}])
    
-.controller('myLocationCtrl', function($scope) {
+.controller('myLocationCtrl',   function($scope, getCurrentPosition) {    
+    getCurrentPosition(function (position) {
+           
+            var lat =  position.coords.latitude;
+           var lng =  position.coords.longitude;
+            var accuracy =   position.coords.accuracy;
 
+           // set map here
+           var map = L.map('map').setView([lat, lng], 13);
+           L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+               attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+               maxZoom: 18
+           }).addTo(map);
+
+           L.marker([lat, lng]).addTo(map).bindPopup("You are here!").openPopup();
+           L.circle([lat, lng], accuracy).addTo(map);
+     
+    
+    }); 
 })
    
 .controller('myChannelCtrl', function($scope) {
@@ -13,10 +53,11 @@ angular.module('app.controllers', [])
 })
    
 .controller('loginCtrl', function($scope) {
-
+    // $scope.username = 'test';
 })
    
 .controller('signupCtrl', function($scope) {
 
-})
+})//
+
  
--- MyApp/www/js/services.js
+++ MyApp/www/js/services.js
@@ -1,10 +1,47 @@
-angular.module('app.services', [])
+angular.module('app.services',[]); 
+ 
+//
+angular.module('app.services.Cordova', [])
+.factory('deviceReady', function () {
+//    return function (done) {
+//        if (typeof window.cordova === 'object') {
+//            document.addEventListener('deviceready', function () { 
+//                done();
+//            }, false);
+//        } else {
+//            done();
+//
+//        }
+//    };
+});
 
-.factory('BlankFactory', [function(){
+angular.module('app.services.Geolocation', []) 
+.factory('getCurrentPosition', function () {
+    return function (done) { 
+            navigator.geolocation.getCurrentPosition(function (position) {                 
+                    done(position);
+                
+            }, function (error) {                
+                    throw new Error('Unable to retreive position');
+            });
+    };
+ 
+});
+//
+angular.module('app.services.Forecast', [])
+.factory('getWeather', function ($http) {
+    
+    
+    return function (position, done) {
+         console.log('In weather function');
+        $http({ method: 'GET', url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + "&appid=44db6a862fba0b067b1930da0d769e98&units=metric" })
+            .success(function (data, status, headers, config) {
+                done(data, position);
+            })
+            .error(function (data, status, headers, config) {
+                throw new Error('Unable to get weather');
+            });
+    };
+});
 
-}])
-
-.service('BlankService', [function(){
-
-}]);
-
+ 
